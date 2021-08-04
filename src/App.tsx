763fac50a6afb2816e2a6c9c513b81ab72c4680c
EC2 Default User <ec2-user@ip-172-31-95-55.ec2.internal>
2021-08-04 18:54:02 +0000

    Added license to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
 import React from "react";
 import Search from "./search/Search";
 import { facetConfiguration } from "./search/configuration";
