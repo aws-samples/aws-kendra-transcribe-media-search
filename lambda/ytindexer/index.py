a00b43e2e32b41f0ae8fa7a66b3a518f0576f30c
Roshan Thomas <roshtho@amazon.com>
2024-01-16 15:44:13 +1100

    Use yt_dlp python package instead of pytube and using aws cloudformation package

--- lambda/ytindexer/index.py
+++ lambda/ytindexer/index.py
@@ -8,9 +8,9 @@ import boto3
 import logging
 import cfnresponse
 import time
-
-from urllib.parse import urlparse
-from urllib.parse import parse_qs
+import yt_dlp
+from datetime import datetime
+from botocore.exceptions import ClientError
 
 LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO').upper()
 logger = logging.getLogger()
@@ -20,8 +20,7 @@ logger.setLevel(LOG_LEVEL)
 retryceil=int(os.environ['RETRY'])
 
 sys.path.insert(1, '/tmp/')
-from pytube import YouTube
-from pytube import Playlist
+
 dynamodb = boto3.resource('dynamodb')
 ytcommonURL='https://www.youtube.com/watch?v='
 
@@ -33,21 +32,6 @@ mediaBucket = os.environ['mediaBucket']
 SAVE_PATH = "/tmp"
 mediaFolderPrefix = os.environ['mediaFolderPrefix']
 metaDataFolderPrefix = os.environ['metaDataFolderPrefix']+mediaFolderPrefix
-
-
-def ytvideoid(value):
-    query = urlparse(value)
-    if query.hostname == 'youtu.be':
-        return query.path[1:]
-    if query.hostname in ('www.youtube.com', 'youtube.com'):
-        if query.path == '/watch':
-            p = parse_qs(query.query)
-            return p['v'][0]
-        if query.path[:7] == '/embed/':
-            return query.path.split('/')[2]
-        if query.path[:3] == '/v/':
-            return query.path.split('/')[2]
-    return None
 
 def exit_status(event, context, status):
     logger.info(f"exit_status({status})")
@@ -61,79 +45,52 @@ def exit_status(event, context, status):
                cfnresponse.send(event, context, status, {}, None)
     return status
 
-def downloadYTAudio(event,context,ytkey,url):
-    returnVal = 0
-    ytVideoURL = ytcommonURL+ytkey
-    yt = YouTube(ytVideoURL)
-    logger.info('Downloading Youtube Audio for ->'+ytVideoURL)
-    audio_name=ytkey
-    try:
-        yt.streams.filter(only_audio=True).first().download(SAVE_PATH,audio_name+'.mp3')
-        logger.info(yt.metadata)
-    except Exception as e:
-        statusCode=500
-        body='ERROR: Could not download Audio from YouTube->'+str(e)
-        logger.error(body)
-        returnVal = 1
-        return returnVal
-
-    try:
-        s3_client = boto3.client('s3', region) 
-        logger.info('Uploading to s3 media bucket ->'+mediaFolderPrefix+audio_name+'.mp3')
-        s3_client.upload_file(SAVE_PATH+'/'+audio_name+'.mp3', mediaBucket, mediaFolderPrefix+audio_name+'.mp3')
-    except Exception as e:
-        body='ERROR: Could not upload Audio to S3->'+str(e)
-        logger.error(body)
-        returnVal = 2
-        return returnVal
-
-    try:
-        returnVal = updateDDBTable(event,context,ytkey, yt.author, yt.length, yt.publish_date, yt.views,ytVideoURL, yt.title,url)
-    except Exception as e:
-        body='ERROR: Could not update DynamoDB table YTMediaDDBQueueTable->'+str(e)
-        logger.error(body)
-        returnVal = 3
-        return returnVal
- 
-    return returnVal
-    
-def updateDDBTable(event,context,ytkey,author,video_length,publish_date,view_count,source_uri,title,url):
+def updateDDBTable(videoMetaData):    
     tableName = os.environ['ddbTableName']
     table = dynamodb.Table(tableName)
-    try:
-        response = table.put_item(
-                Item={
-                        'ytkey': ytkey,
-                        'downloaded': True,
-                        'ytauthor': author,
-                        'video_length': video_length,
-                        'publish_date': publish_date.isoformat(),
-                        'view_count': view_count,
-                        'source_uri': source_uri,
-                        'title': title 
-                    },
-                    ConditionExpression='attribute_not_exists(ytkey)'
-            )            
-    except Exception as e:
-        if e.response['Error']['Code']=='ConditionalCheckFailedException':  
-            logger.info("Youtube Video "+url+" has already been indexed")
-        else:
-            logger.error('ERROR: Could not index video '+url+' ->'+str(e))
-            return 4
-            
-    try:
-        json_dump = json.dumps({'Attributes': {'_source_uri':source_uri, '_category':'YouTube video', '_created_at':publish_date.isoformat(),'video_length':video_length,'video_view_count':view_count,'ytauthor':author,'ytsource':source_uri},
-                            'Title': title
-                            })
-        encoded_string = json_dump.encode("utf-8")
-        file_name = metaDataFolderPrefix+ytkey+".mp3.metadata.json"
-        s3_path = file_name
-        s3 = boto3.resource("s3")
-        logger.info('Uploading to s3 media bucket ->'+metaDataFolderPrefix+ytkey+'.mp3.metadata.json')
-        s3.Bucket(mediaBucket).put_object(Key=s3_path, Body=encoded_string)
-    except Exception as e:
-        logger.error("Could not upload the metadata json to S3" + str(e) )
-        return 5
+    for video in videoMetaData['entries']:
+        video_id = video['id']
+        title = video['title']
+        uploader = video['uploader'] 
+        upload_date = video['upload_date']
+        duration = video['duration']
+        view_count = video['view_count']  
+        date_obj = datetime.strptime(upload_date, "%Y%m%d")
+        iso_upload_date = date_obj.isoformat()
+
+        try:
+            response = table.put_item(
+                    Item={
+                            'ytkey': video_id,
+                            'downloaded': True,
+                            'ytauthor': uploader,
+                            'video_length': duration,
+                            'publish_date': iso_upload_date,
+                            'view_count': view_count,
+                            'source_uri': ytcommonURL+video_id,
+                            'title': title 
+                        },
+                        ConditionExpression='attribute_not_exists(ytkey)'
+                )            
+        except ClientError as e:
+            if e.response['Error']['Code']=='ConditionalCheckFailedException':  
+                logger.info("Youtube Video "+ytcommonURL+video_id+" has already been indexed")
+        except Exception as e:
+            logger.error('ERROR: Could not index video '+ytcommonURL+video_id+' ->'+str(e))
+            return 1
+        try:
+            json_dump = json.dumps({'Attributes': {'_source_uri':ytcommonURL+video_id, '_category':'YouTube video', '_created_at':iso_upload_date,'video_length':duration,'video_view_count':view_count,'ytauthor':uploader,'ytsource':ytcommonURL+video_id},
+                                'Title': title
+                                })
+            encoded_string = json_dump.encode("utf-8")
+            file_name = metaDataFolderPrefix+video_id+".mp3.metadata.json"
+            s3_path = file_name
+            s3 = boto3.resource("s3")
+            logger.info('Uploading to s3 media bucket ->'+metaDataFolderPrefix+video_id+'.mp3.metadata.json')
+            s3.Bucket(mediaBucket).put_object(Key=s3_path, Body=encoded_string)
+        except Exception as e:
+            logger.error("Could not upload the metadata json to S3" + str(e) )
+            return 2
     return 0
 
 def empty_bucket(mediaBucket,event, context):
@@ -163,30 +120,43 @@ def lambda_handler(event, context):
 
     region = os.environ['AWS_REGION']
     numberOfYTVideos = int(os.environ['numberOfYTVideos'])
-    
-    ytPlayList = Playlist(playListURL)
-    logger.info('Number of videos in the playlist->'+str(len(ytPlayList.video_urls)))
-    endoflist=len(ytPlayList.video_urls) if len(ytPlayList.video_urls)<numberOfYTVideos else numberOfYTVideos
-    
-    for url in ytPlayList.video_urls[:endoflist]:     
-        logger.info("Checking Youtube Video "+url)
-        retrycount = 1
-        videoid=ytvideoid(url)
+    ydl_opts = {
+            'format': 'bestaudio',
+            'outtmpl': SAVE_PATH+'/%(id)s.%(ext)s',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+            'playlistend': numberOfYTVideos
+            }
+            
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         try:
-            while True:
-                returnVal = downloadYTAudio(event,context,videoid,url)
-                if (returnVal == 0 or retrycount > retryceil):
-                    break
-                else:
-                    # Sleep for 5 seconds before retries
-                    logger.info("Sleeping for 2 seconds and Retrying downloadYTAudio "+str(retrycount)+" out of " + str(retryceil))
-                    time.sleep(2)
-                    retrycount += 1
-            if (returnVal != 0):
-                logger.error("Failure while downloading YT Video, uploading to S3 and indexing into DynamoDB table. See errors above.")
-                return exit_status(event, context, cfnresponse.FAILED)
+            ydl.download([playListURL])
         except Exception as e:
-            logger.error('ERROR: Could not index video '+url+' ->'+str(e))
+            logger.error('ERROR: Could not index videos from playlist ->'+str(e))
             return exit_status(event, context, cfnresponse.FAILED)
-
+        try:
+            videoMetaData = ydl.extract_info(playListURL, download=False)
+        except Exception as e:
+            logger.error('ERROR: Could not extract metadata from YT videos ->'+str(e))
+            return exit_status(event, context, cfnresponse.FAILED)
+        for video in videoMetaData['entries']:
+            video_id = video['id']
+            # Upload downloaded mp3 files to S3
+            try:
+                s3_client = boto3.client('s3', region) 
+                logger.info('Uploading to s3 media bucket ->'+mediaFolderPrefix+video_id+'.mp3')
+                s3_client.upload_file(SAVE_PATH+'/'+video_id+'.mp3', mediaBucket, mediaFolderPrefix+video_id+'.mp3')
+            except Exception as e:
+                body='ERROR: Could not upload Audio to S3->'+str(e)
+                logger.error(body)
+            # Update the DynamoDB table    
+            try:
+                returnVal = updateDDBTable(videoMetaData)
+            except Exception as e:
+                logger.error('ERROR: Could not update DDB'+str(e))
+                return exit_status(event, context, cfnresponse.FAILED)
+            
     return exit_status(event, context, cfnresponse.SUCCESS)
